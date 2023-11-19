150e1692f230fc08a1c1ad5a5da4b8a9466a4a2b
Alvaro Candelario <alvarcande@gmail.com>
2023-11-19 12:12:28 +0100

    first commit

---
+++ exam_rank_02/first_word.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   first_word.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: acandela <acandela@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/19 11:26:01 by acandela          #+#    #+#             */
+/*   Updated: 2023/11/19 12:05:33 by acandela         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include <unistd.h>
+
+int	main(int argc, char **argv)
+{
+	int		i;
+	char	salto;
+
+	salto = '\n';
+	i = 0;
+	if (argc == 2)
+	{
+		while (argv[argc - 1][i] == ' ' || argv[argc - 1][i] == 9)
+			i++;
+		// if (argv[argc - 1][i] == ' ' || argv[argc - 1][i] == 9)
+		// 	i++;
+		while (argv[argc - 1][i] != '\0' && argv[argc - 1][i] != ' ')
+                // && argv[argc - 1][i] == 9)
+		{
+			write(1, &argv[argc - 1][i], 1);
+			i++;
+		}
+	}
+	write(1, &salto, 1);
+	return (0);
+}
